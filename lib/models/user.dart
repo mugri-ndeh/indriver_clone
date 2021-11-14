f5a14fab9725f25227950f4687a67ba0c2879940
mugri-ndeh <mugrindeh@gmail.com>
2021-11-14 08:08:16 +0100

    user model

---
+++ lib/models/user.dart
@@ -0,0 +1,16 @@
+class User {
+  int? _id;
+  String? _firstName;
+  String? _phoneNUmber;
+  bool? isOnline;
+  bool? isDriver;
+
+  String? _lastName;
+  String? _email;
+  String? _token;
+  String? _photo;
+
+  int? _votes;
+  int? _trips;
+  double? _rating;
+}
