a11a96ba9b050581c173b268eb1cb94d67a2f200
mugri-ndeh <mugrindeh@gmail.com>
2021-11-14 08:07:53 +0100

    new appBar

--- lib/ui/app_bar.dart
+++ lib/ui/app_bar.dart
@@ -39,5 +39,34 @@ class HomeAppBar extends StatelessWidget with PreferredSizeWidget {
 
   @override
   // TODO: implement preferredSize
-  Size get preferredSize => Size.fromHeight(100);
+  Size get preferredSize => const Size.fromHeight(100);
+}
+
+class NavAppbar extends StatelessWidget with PreferredSizeWidget {
+  const NavAppbar({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    print(title);
+    return AppBar(
+      title: Text(title + 'hry'),
+      backgroundColor: Colors.white,
+      leading: Builder(builder: (context) {
+        return IconButton(
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+          },
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.green,
+          ),
+        );
+      }),
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => const Size.fromHeight(60);
 }
