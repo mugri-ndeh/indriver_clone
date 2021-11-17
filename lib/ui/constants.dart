c7da5a1dc98a814adb258f796ccae67e0614fd78
mugri-ndeh <mugrindeh@gmail.com>
2021-11-17 19:21:05 +0100

    added constants to be used through out

---
+++ lib/ui/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0XFF023e8a);
+//const Color buttonColor = Colors.green;
+const Color startMarker = Colors.green;
+const Color endMarker = Colors.blue;
+const Color buttonColor = Color(0XFF03045e);
+const Color notSelected = Colors.grey;
