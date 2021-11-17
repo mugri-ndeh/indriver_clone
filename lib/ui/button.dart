4cfd977bf1ac1ecf7eda512ec0705839c38fd845
mugri-ndeh <mugrindeh@gmail.com>
2021-11-17 19:21:21 +0100

    created custom button

--- lib/ui/button.dart
+++ lib/ui/button.dart
@@ -1,13 +1,31 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:indriver_clone/ui/constants.dart';
 
-class BotButton extends StatelessWidget {
-  const BotButton({Key? key}) : super(key: key);
+class BotButton extends StatefulWidget {
+  const BotButton({Key? key, required this.onTap, required this.title})
+      : super(key: key);
 
+  final Function() onTap;
+  final String title;
+
+  @override
+  State<BotButton> createState() => _BotButtonState();
+}
+
+class _BotButtonState extends State<BotButton> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(16)),
+    return GestureDetector(
+      onTap: widget.onTap,
+      child: Container(
+        width: size.width * 0.9,
+        child: Text(widget.title, style: const TextStyle(color: Colors.white)),
+        decoration: BoxDecoration(
+            color: buttonColor, borderRadius: BorderRadius.circular(16)),
+      ),
     );
   }
 }
