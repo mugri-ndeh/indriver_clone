f2e598d7a9695c704edb15e40909f298184d3462
mugri-ndeh <mugrindeh@gmail.com>
2021-11-17 06:43:45 +0100

    authentication provider init

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 
 class Authentication with ChangeNotifier {
-  login() {}
+  login(String phoneNumber) async {
+    try {
+      var auth = FirebaseAuth.instance;
+      await auth.signInWithPhoneNumber(phoneNumber);
+    } catch (e) {}
+  }
+
   logout() {}
   register() {}
+  completeprofile() {}
 }
