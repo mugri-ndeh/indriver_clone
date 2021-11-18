ae94e5088a1cae726953bdbded039109f16052f0
mugri-ndeh <mugrindeh@gmail.com>
2021-11-18 06:41:40 +0100

    basic layout

--- lib/driver/screens/rating.dart
+++ lib/driver/screens/rating.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:indriver_clone/ui/constants.dart';
 
 class Ratings extends StatefulWidget {
   Ratings({Key? key}) : super(key: key);
@@ -12,16 +13,37 @@ class _RatingsState extends State<Ratings> with AutomaticKeepAliveClientMixin {
   bool get wantKeepAlive => true;
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(),
-            const Padding(
-              padding: EdgeInsets.all(5),
-            ),
-            Container(),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Center(
+                child: Container(
+                  height: 200,
+                  width: size.width * 0.95,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: primaryColor,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(5),
+              ),
+              Center(
+                child: Container(
+                  height: 100,
+                  width: size.width * 0.95,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: primaryColor),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
