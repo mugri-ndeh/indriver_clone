6109af4b01462706c54b09e33b0d90b2bcd5461e
mugri-ndeh <mugrindeh@gmail.com>
2021-11-18 06:43:55 +0100

    starting point

---
+++ lib/screens/root.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:indriver_clone/main.dart';
+import 'package:indriver_clone/models/user.dart';
+import 'package:indriver_clone/providers/auth.dart';
+import 'package:indriver_clone/screens/homepage.dart';
+import 'package:provider/provider.dart';
+
+class Root extends StatelessWidget {
+  const Root({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final auth = Provider.of<Authentication>(context, listen: false);
+
+    return StreamBuilder<UserModel>(
+      stream: auth.onAuthStateChanged(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final user = snapshot.data;
+          return user != null ? HomePage() : const Login();
+        }
+        return const Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
+    );
+  }
+}
