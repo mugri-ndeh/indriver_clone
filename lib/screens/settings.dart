50efa6159938ce1625758954dc3fea640ea72692
mugri-ndeh <mugrindeh@gmail.com>
2021-11-22 00:45:43 +0100

    dummy

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,41 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:indriver_clone/providers/auth.dart';
 import 'package:indriver_clone/ui/app_bar.dart';
 import 'package:indriver_clone/ui/nav_drawer.dart';
+import 'package:provider/provider.dart';
 
-class Settings extends StatelessWidget {
-  const Settings({Key? key}) : super(key: key);
+class Setting extends StatelessWidget {
+  const Setting({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    //var provider = Provider.of<Authentication>(context, listen: false);
     return Scaffold(
       drawer: const NavDrawer(),
-      appBar: const NavAppbar(title: 'Settings'),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const ListTile(
-              title: Text('Change phone number'),
-              subtitle: Text('+237xxxxxxx'),
-              trailing: Icon(Icons.arrow_forward_ios),
-            ),
-            const ListTile(
-              title: Text('Language'),
-              subtitle: Text('English'),
-              trailing: Icon(Icons.arrow_forward_ios),
-            ),
-            const ListTile(
-              title: Text('About application'),
-              subtitle: Text('+237xxxxxxx'),
-              trailing: Icon(Icons.arrow_forward_ios),
-            ),
-            ListTile(
-              title: const Text(
-                'Logout',
-                style: TextStyle(color: Colors.green),
+      // appBar: const NavAppbar(title: 'Settings'),
+      body: Consumer<Authentication>(
+        builder: (_, provider, __) => SingleChildScrollView(
+          child: Column(
+            children: const [
+              ListTile(
+                title: Text('Phone number'),
+                subtitle: Text('+237xxxxxxx'),
+                trailing: Icon(Icons.arrow_forward_ios),
               ),
-              onTap: () {},
-            ),
-          ],
+              ListTile(
+                title: Text('Language'),
+                subtitle: Text('English'),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+              ListTile(
+                title: Text('About application'),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+            ],
+          ),
         ),
       ),
     );
