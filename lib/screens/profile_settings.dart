4277e8fd65fcee851a2161c527e61e54607f91a8
mugri-ndeh <mugrindeh@gmail.com>
2021-11-22 00:47:54 +0100

    user can edit profile

--- lib/screens/profile_settings.dart
+++ lib/screens/profile_settings.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:date_field/date_field.dart';
+import 'package:indriver_clone/providers/auth.dart';
 import 'package:indriver_clone/ui/button.dart';
 import 'package:indriver_clone/ui/constants.dart';
+import 'package:provider/provider.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
@@ -11,7 +13,15 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  var _formKey = GlobalKey<FormState>();
   DateTime? selectedDate;
+  final _firstnameController = TextEditingController();
+
+  final _lastNameController = TextEditingController();
+
+  final _emailController = TextEditingController();
+
+  final _usernameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -32,67 +42,91 @@ class _ProfileState extends State<Profile> {
           onPressed: () => Navigator.pop(context),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
-        child: SingleChildScrollView(
-          child: Form(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  height: size.height * 0.8,
-                  child: Column(
-                    children: [
-                      const Center(
-                        child: CircleAvatar(
-                          backgroundColor: primaryColor,
-                          child: Icon(
-                            Icons.person,
-                            size: 80,
+      body: Consumer<Authentication>(
+        builder: (_, provider, __) => Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: size.height * 0.8,
+                    child: Column(
+                      children: [
+                        const Center(
+                          child: CircleAvatar(
+                            backgroundColor: primaryColor,
+                            child: Icon(
+                              Icons.person,
+                              size: 80,
+                            ),
+                            radius: 80,
                           ),
-                          radius: 80,
                         ),
-                      ),
-                      TextFormField(
-                        initialValue: 'Frunwi',
-                        decoration: const InputDecoration(
-                            hintText: 'First name',
-                            prefixIcon: Icon(Icons.person_outline)),
-                      ),
-                      TextFormField(
-                        initialValue: 'Mugri Ndeh',
-                        decoration: const InputDecoration(
-                            hintStyle: TextStyle(fontWeight: FontWeight.bold),
-                            hintText: 'First name',
-                            prefixIcon: Icon(Icons.person_outline)),
-                      ),
-                      DateTimeField(
-                          mode: DateTimeFieldPickerMode.date,
-                          decoration: const InputDecoration(
-                              prefixIcon: Icon(Icons.calendar_today),
-                              hintText: 'Date of Birth'),
-                          selectedDate: selectedDate,
-                          onDateSelected: (DateTime value) {
-                            setState(() {
-                              selectedDate = value;
-                            });
-                          }),
-                      TextFormField(
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: const InputDecoration(
-                            hintText: 'Email',
-                            prefixIcon: Icon(Icons.mail_outline)),
-                      ),
-                      TextFormField(
-                        initialValue: 'Buea',
-                        decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.location_city_outlined)),
-                      ),
-                    ],
+                        TextFormField(
+                          controller: _firstnameController,
+                          //initialValue: provider.loggedUser.firstName,
+                          decoration: InputDecoration(
+                              hintText: provider.loggedUser.firstName,
+                              prefixIcon: const Icon(Icons.person_outline)),
+                        ),
+                        TextFormField(
+                          controller: _lastNameController,
+                          // initialValue: provider.loggedUser.lastName,
+                          decoration: InputDecoration(
+                              hintStyle:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                              hintText: provider.loggedUser.lastName,
+                              prefixIcon: const Icon(Icons.person_outline)),
+                        ),
+                        TextFormField(
+                          controller: _usernameController,
+                          //initialValue: provider.loggedUser.username,
+                          decoration: InputDecoration(
+                              hintStyle:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                              hintText: provider.loggedUser.username,
+                              prefixIcon: const Icon(Icons.person_outline)),
+                        ),
+                        DateTimeField(
+                            mode: DateTimeFieldPickerMode.date,
+                            decoration: InputDecoration(
+                                prefixIcon: const Icon(Icons.calendar_today),
+                                hintText: provider.loggedUser.dob),
+                            selectedDate: selectedDate,
+                            onDateSelected: (DateTime value) {
+                              setState(() {
+                                selectedDate = value;
+                              });
+                            }),
+                        TextFormField(
+                          controller: _emailController,
+                          //initialValue: provider.loggedUser.email,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                              hintText: provider.loggedUser.email,
+                              prefixIcon: const Icon(Icons.mail_outline)),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                BotButton(onTap: () {}, title: 'Save')
-              ],
+                  BotButton(
+                      onTap: () {
+                        if (_formKey.currentState!.validate()) {
+                          provider.updateProfile(
+                              _firstnameController.text,
+                              _lastNameController.text,
+                              _usernameController.text,
+                              selectedDate.toString(),
+                              _emailController.text,
+                              context);
+                        }
+                      },
+                      title: 'Save')
+                ],
+              ),
             ),
           ),
         ),
