4e5bb0203ec792c64da45c85327029e5b92f59d2
mugri-ndeh <mugrindeh@gmail.com>
2021-11-22 01:07:52 +0100

    removed coments

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,18 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:indriver_clone/providers/auth.dart';
 import 'package:indriver_clone/providers/handle.dart';
 import 'package:indriver_clone/screens/search_screen.dart';
+import 'package:indriver_clone/screens/send_requests.dart';
+import 'package:indriver_clone/screens/userprogressmap.dart';
 import 'package:indriver_clone/ui/app_bar.dart';
 import 'package:indriver_clone/ui/button.dart';
 import 'package:indriver_clone/ui/constants.dart';
 import 'package:indriver_clone/ui/nav_drawer.dart';
 import 'package:flutter_sim_country_code/flutter_sim_country_code.dart';
 import 'package:indriver_clone/util/assistant_methods.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -20,11 +25,15 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final LatLng _center = const LatLng(45.521563, -122.677433);
+  var request;
 
   void _onMapCreated(GoogleMapController controller) {
     var provider = Provider.of<AppHandler>(context, listen: false);
+    var prov = Provider.of<Authentication>(context, listen: false);
     provider.mapController = controller;
     provider.locatePosition(context);
+    print('Currency');
+    currency();
   }
 
   GoogleMapController? newController;
@@ -37,8 +46,16 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    request = getRequests();
+  }
+
+  @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    var auth = Provider.of<Authentication>(context, listen: false);
     return Scaffold(
       drawer: const NavDrawer(),
       extendBodyBehindAppBar: true,
@@ -88,13 +105,6 @@ class _HomePageState extends State<HomePage> {
                                     ? startMarker
                                     : notSelected,
                               ),
-                              // title: TextFormField(
-                              //   enabled: false,
-                              //   focusNode: FocusNode(canRequestFocus: true),
-                              //   initialValue: 'wow',
-                              //   decoration: const InputDecoration(
-                              //       hintText: 'Pickup location'),
-                              // ),
                               title: Text(location.startpoint),
                             ),
                           ),
@@ -110,17 +120,6 @@ class _HomePageState extends State<HomePage> {
                                     ? endMarker
                                     : notSelected,
                               ),
-                              // title: TextFormField(
-                              //   focusNode: FocusNode(canRequestFocus: true),
-                              //   //enabled: false,
-                              //   // initialValue: location.destinationLocation ==
-                              //   //         null
-                              //   //     ? ''
-                              //   //     : location.destinationLocation!.placeName,
-                              //   initialValue: location.dummy,
-                              //   decoration: const InputDecoration(
-                              //       hintText: 'Destination'),
-                              // ),
                               title: Stack(children: [
                                 Text(location.endpoint),
                                 const Positioned(
@@ -152,11 +151,56 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
-            BotButton(
-                onTap: () {
-                  makeRequest();
-                },
-                title: 'Request a vehicle')
+            Consumer<AppHandler>(
+              builder: (_, loc, __) => BotButton(
+                  onTap: () {
+                    print(loc.endpoint);
+                    if (loc.endpoint != 'End point') {
+                      loc.sendRequest(
+                          loc.pickupLocation!.latitude!.toString(),
+                          loc.pickupLocation!.longitude!.toString(),
+                          false,
+                          auth.loggedUser.username!,
+                          DateTime.now().toString(),
+                          '672617465',
+                          loc.pickupLocation!.placeName!,
+                          loc.destinationLocation!.placeName!,
+                          loc.destinationLocation!.latitude!.toString(),
+                          loc.destinationLocation!.longitude!.toString(),
+                          '500');
+
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SendingRequests(),
+                        ),
+                      );
+                    } else {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          content: SizedBox(
+                            height: 150,
+                            width: size.width * 0.6,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text('Please select a drop off location'),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text('OK'),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                  title: 'Request a vehicle'),
+            )
           ],
         ),
       ),
@@ -166,10 +210,62 @@ class _HomePageState extends State<HomePage> {
   showBottom(BuildContext context, Size size, bool pickup) {
     showModalBottomSheet(
       elevation: 1,
+      isDismissible: false,
+      enableDrag: false,
       context: context,
       builder: (context) => SearchScreen(pickup: pickup),
     );
   }
 
-  void makeRequest() {}
+  void currency() {
+    Locale locale = Localizations.localeOf(context);
+    var format = NumberFormat.simpleCurrency(locale: locale.toString());
+    print("CURRENCY SYMBOL ${format.currencySymbol}"); // $
+    print("CURRENCY NAME ${format.currencyName}"); // USD
+  }
+
+  Future<bool> _cancelRequest() async {
+    //loc.removeRequest();
+    return true;
+  }
+
+  Stream<QuerySnapshot>? getRequests() {
+    var provider = Provider.of<Authentication>(context, listen: false);
+    try {
+      return FirebaseFirestore.instance
+          .collection('request')
+          .where('accepted', isEqualTo: false)
+          .where('id', isEqualTo: provider.auth.currentUser!.uid)
+          .snapshots();
+    } catch (e) {
+      return null;
+    }
+    //print(requests[0].username);
+  }
+
+  void acceptRequest(context) async {
+    var currentUser =
+        Provider.of<Authentication>(context, listen: false).auth.currentUser;
+    await FirebaseFirestore.instance
+        .collection('request')
+        .doc(currentUser!.uid)
+        .update({
+      'accepted': true,
+    }).then((value) {
+      print('Waiting for Driver');
+    });
+  }
+
+  void declineRequest(context) async {
+    var currentUser =
+        Provider.of<Authentication>(context, listen: false).auth.currentUser;
+    await FirebaseFirestore.instance
+        .collection('request')
+        .doc(currentUser!.uid)
+        .update({
+      'driverId': "",
+    }).then((value) {
+      print('Waiting for Driver');
+    });
+  }
 }
