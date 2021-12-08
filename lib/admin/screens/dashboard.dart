c60c14dfb52f17cb937d15679e2b3bc0b75926fb
mugri-ndeh <mugrindeh@gmail.com>
2021-12-08 01:34:06 +0100

    dashboard to see all drivers pending approval

---
+++ lib/admin/screens/dashboard.dart
@@ -0,0 +1,121 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:indriver_clone/admin/screens/monitor_drivers.dart';
+import 'package:indriver_clone/admin/screens/verify_screen.dart';
+import 'package:indriver_clone/admin/util/data.dart';
+import 'package:indriver_clone/models/user.dart';
+import 'package:indriver_clone/ui/button.dart';
+import 'package:indriver_clone/ui/constants.dart';
+
+class Dashboard extends StatefulWidget {
+  Dashboard({Key? key}) : super(key: key);
+
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  var start;
+  Stream<QuerySnapshot?> allUsers() {
+    return FirebaseFirestore.instance
+        .collection('users')
+        .where('submittedStatus', isEqualTo: 'waiting')
+        .snapshots();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    start = allUsers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: Drawer(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MonitorDrivers(),
+                      ),
+                    );
+                  },
+                  child: ListTile(
+                    title: const Text('verified users'),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        appBar: AppBar(
+          title: const Text('Dashboard'),
+        ),
+        body: StreamBuilder<QuerySnapshot?>(
+            stream: start,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              if (!snapshot.hasData) {
+                return const Center(
+                  child: Text('No unverified Users'),
+                );
+              } else {
+                List<UserModel> userList = snapshot.data!.docs
+                    .map((e) => UserModel.fromSnapshot(e))
+                    .toList();
+                return ListView.builder(
+                  itemCount: userList.length,
+                  itemBuilder: (context, index) => Container(
+                      height: 100,
+                      decoration:
+                          BoxDecoration(color: Colors.white, boxShadow: [
+                        BoxShadow(
+                          offset: const Offset(0, 10),
+                          blurRadius: 5,
+                          color: primaryColor.withOpacity(0.2),
+                        ),
+                      ]),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 8.0, horizontal: 16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(userList[index].username!),
+                                Text(userList[index].phoneNumber!)
+                              ],
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (copntext) => VerifyDriver(
+                                      user: userList[index],
+                                    ),
+                                  ),
+                                );
+                              },
+                              child: const Text('View Details'),
+                            ),
+                          ],
+                        ),
+                      )),
+                );
+              }
+            }));
+  }
+}
