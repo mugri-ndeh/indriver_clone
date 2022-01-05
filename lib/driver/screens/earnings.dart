07cadca03fcc90115ea8bbc5aba4ff86a39dfdfa
mugri-ndeh <mugrindeh@gmail.com>
2022-01-05 09:24:08 +0100

    drivers personal earning

--- lib/driver/screens/earnings.dart
+++ lib/driver/screens/earnings.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:indriver_clone/providers/auth.dart';
+import 'package:provider/provider.dart';
 
 class Earnings extends StatefulWidget {
-  Earnings({Key? key}) : super(key: key);
+  const Earnings({Key? key}) : super(key: key);
 
   @override
   _EarningsState createState() => _EarningsState();
@@ -9,17 +13,52 @@ class Earnings extends StatefulWidget {
 
 class _EarningsState extends State<Earnings>
     with AutomaticKeepAliveClientMixin {
+  var earnings;
+
+  Stream<QuerySnapshot> getEarnings() {
+    var id = Provider.of<Authentication>(context, listen: false).loggedUser.id;
+    return FirebaseFirestore.instance
+        .collection('earnings')
+        .where('driverId', isEqualTo: id)
+        .snapshots();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    earnings = getEarnings();
+  }
+
   @override
   bool get wantKeepAlive => true;
   @override
   Widget build(BuildContext context) {
-    // ignore: prefer_const_constructors
     return Scaffold(
-      // ignore: prefer_const_constructors
-      body: Center(
-        // ignore: prefer_const_constructors
-        child: Text('No earings yet'),
-      ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: earnings,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.data != null) {
+              return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) => ListTile(
+                  title: Text(
+                    'Amount: ${snapshot.data!.docs[index]['amount']}KES',
+                  ),
+                  subtitle: Text(
+                    'Date: ${snapshot.data!.docs[index]['date'].toDate()}',
+                  ),
+                ),
+              );
+            } else {
+              return const Center(
+                // ignore: prefer_const_constructors
+                child: Text('No earings yet'),
+              );
+            }
+          }),
     );
   }
 }
