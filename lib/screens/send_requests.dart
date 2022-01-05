508eeb4dbc4a851e79e54e067a9e4968a0d9e043
mugri-ndeh <mugrindeh@gmail.com>
2022-01-05 09:22:45 +0100

    live interaction between driver and passenger

--- lib/screens/send_requests.dart
+++ lib/screens/send_requests.dart
@@ -2,8 +2,13 @@ import 'dart:ffi';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:indriver_clone/models/requests.dart';
+import 'package:indriver_clone/models/user.dart';
 import 'package:indriver_clone/providers/auth.dart';
 import 'package:indriver_clone/providers/handle.dart';
+import 'package:indriver_clone/screens/homepage.dart';
+import 'package:indriver_clone/screens/userprogressmap.dart';
 import 'package:indriver_clone/ui/button.dart';
 import 'package:provider/provider.dart';
 
@@ -17,10 +22,13 @@ class SendingRequests extends StatefulWidget {
 class _SendingRequestsState extends State<SendingRequests> {
   var request;
 
+  UserModel? user;
+
   @override
   void initState() {
     super.initState();
     request = getRequests();
+    user = Provider.of<Authentication>(context, listen: false).loggedUser;
   }
 
   @override
@@ -30,51 +38,87 @@ class _SendingRequestsState extends State<SendingRequests> {
     return Scaffold(
       body: WillPopScope(
         onWillPop: () async {
-          loc.removeRequest();
-          return true;
+          // loc.removeRequest();
+          // Navigator.pushAndRemoveUntil(
+          //     context,
+          //     MaterialPageRoute(
+          //       builder: (context) => const HomePage(),
+          //     ),
+          //     (route) => false);
+          return false;
         },
         child: StreamBuilder<QuerySnapshot>(
-            stream: request,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-              if (snapshot.data == null) {
-                return const Center();
-              }
-              var request = snapshot.data!.docs.first;
-              if (request['driverId'] == "") {
-                return Center(
-                  child: SizedBox(
-                    height: 200,
-                    width: size.width * 0.7,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Center(
-                          child: CircularProgressIndicator(),
-                        ),
-                        const Text('Requesting driver Please wait'),
-                        ElevatedButton(
-                          onPressed: () {
-                            loc.removeRequest();
-                            Navigator.pop(context);
-                          },
-                          child: const Text('cancel'),
-                        )
-                      ],
-                    ),
+          stream: request,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.data == null) {
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomePage(),
                   ),
-                );
-              } else {
-                return Container(
-                  child: Center(
+                  (route) => false);
+            }
+            if (snapshot.hasError) {
+              return const Text('Something went wrong.');
+            }
+
+            if (snapshot.data!.docs.isEmpty) {
+              Navigator.pop(context);
+            }
+            if (snapshot.data != null) {
+              print('getting data');
+              var request = snapshot.data!.docs.first;
+              if (request['arrivalTime'].isNotEmpty) {
+                var requestMod =
+                    RideRequest.fromDocument(snapshot.data!.docs.first);
+                if (request['answered'] == true &&
+                    request['accepted'] == true) {
+                  if (request['driverArrived'] == true) {
+                    if (request['journeyStarted']) {
+                      return Center(
+                        child: BotButton(
+                          onTap: () {
+                            var user = Provider.of<Authentication>(context,
+                                    listen: false)
+                                .loggedUser;
+
+                            leaveReview(context, request['driverId'],
+                                user.username!, user.id!, requestMod.price!);
+                          },
+                          title: 'Finish Journney',
+                        ),
+                      );
+                    }
+                    return Center(
+                      child: BotButton(
+                        onTap: () {
+                          startJourney(context);
+                        },
+                        title: 'Start Journey',
+                      ),
+                    );
+                  } else {
+                    return const Center(
+                      child: Text('Waiting for driver to arrive'),
+                    );
+                  }
+                } else {
+                  return Center(
                     child: SizedBox(
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Text(request['driverId']),
+                          Image.network(requestMod.driverPic!),
+                          Text(request['driverName'] +
+                              'will arrive in ' +
+                              request['arrivalTime'] +
+                              'minutes'),
                           BotButton(
                               onTap: () {
                                 acceptRequest(context);
@@ -88,10 +132,59 @@ class _SendingRequestsState extends State<SendingRequests> {
                         ],
                       ),
                     ),
+                  );
+                }
+              }
+              if (request['driverId'] == "") {
+                return Center(
+                  child: SizedBox(
+                    height: 200,
+                    width: size.width * 0.7,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                        const Text('Requesting driver Please wait'),
+                        BotButton(
+                          onTap: () {
+                            loc.removeRequest();
+                          },
+                          title: 'cancel',
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              } else {
+                return Center(
+                  child: SizedBox(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(request['driverName'] + request['arrivalTime']),
+                        BotButton(
+                            onTap: () {
+                              acceptRequest(context);
+                            },
+                            title: 'Accept'),
+                        BotButton(
+                            onTap: () {
+                              declineRequest(context);
+                            },
+                            title: 'Decline')
+                      ],
+                    ),
                   ),
                 );
               }
-            }),
+            } else {
+              return const CircularProgressIndicator();
+            }
+          },
+        ),
       ),
     );
   }
@@ -110,6 +203,7 @@ class _SendingRequestsState extends State<SendingRequests> {
     //print(requests[0].username);
   }
 
+//accept request from driver
   void acceptRequest(context) async {
     var currentUser =
         Provider.of<Authentication>(context, listen: false).auth.currentUser;
@@ -118,22 +212,151 @@ class _SendingRequestsState extends State<SendingRequests> {
         .doc(currentUser!.uid)
         .update({
       'accepted': true,
+      'answered': true,
     }).then((value) {
       print('Waiting for Driver');
     });
   }
 
+//reject request from driver
   void declineRequest(context) async {
     var currentUser =
         Provider.of<Authentication>(context, listen: false).auth.currentUser;
     await FirebaseFirestore.instance
         .collection('request')
         .doc(currentUser!.uid)
-        .update({
-      'accepted': false,
-      'driverId': "",
-    }).then((value) {
+        .update({'accepted': false, 'driverId': "", 'answered': false}).then(
+            (value) {
       print('Waiting for Driver');
     });
+  }
+
+//start journey
+  void startJourney(context) async {
+    var currentUser =
+        Provider.of<Authentication>(context, listen: false).auth.currentUser;
+    await FirebaseFirestore.instance
+        .collection('request')
+        .doc(currentUser!.uid)
+        .update({'journeyStarted': true}).then((value) {
+      print('journey has started');
+    });
+  }
+
+//end ourney
+  void endJourney(context, String id, String amount) async {
+    var currentUser =
+        Provider.of<Authentication>(context, listen: false).auth.currentUser;
+    var loc = Provider.of<AppHandler>(context, listen: false);
+    //add trip
+
+//set earning to driver after journey is completed
+    await FirebaseFirestore.instance.collection('earnings').add({
+      'driverId': id,
+      'amount': amount,
+      'date': DateTime.now(),
+    }).then((value) async {
+      // remove request online
+      loc.removeRequest();
+    });
+  }
+
+//leave review after journey is completed
+
+  var reviewControlller = TextEditingController();
+  void leaveReview(BuildContext context, String id, String username,
+      String userID, String price) {
+    double ratingInit = 3.0;
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => WillPopScope(
+        onWillPop: () async {
+          return false;
+        },
+        child: AlertDialog(
+          content: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Center(
+                    child: Container(
+                      height: 200,
+                      child: TextFormField(
+                        controller: reviewControlller,
+                        decoration:
+                            const InputDecoration(hintText: 'Write a review'),
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(5),
+                  ),
+                  Center(
+                    child: Container(
+                      height: 100,
+                      child: RatingBar.builder(
+                        initialRating: ratingInit,
+                        minRating: 1,
+                        direction: Axis.horizontal,
+                        allowHalfRating: true,
+                        itemCount: 5,
+                        itemPadding:
+                            const EdgeInsets.symmetric(horizontal: 4.0),
+                        itemBuilder: (context, _) => const Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                        ),
+                        onRatingUpdate: (rating) {
+                          ratingInit = rating;
+
+                          print(rating);
+                        },
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
+                  ),
+                  BotButton(
+                      onTap: () async {
+                        await FirebaseFirestore.instance
+                            .collection('users')
+                            .doc(id)
+                            .update({
+                          'trips': FieldValue.increment(1),
+                          'rating': FieldValue.increment(ratingInit),
+                          'earnings': FieldValue.increment(double.parse(price))
+                        });
+                        addreview(id, username, userID,
+                            reviewControlller.text.trim(), ratingInit);
+                        endJourney(context, id, price);
+                        Navigator.pop(context);
+                      },
+                      title: 'Submit Review')
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+//add review function
+  void addreview(String driverId, String username, String userId, String review,
+      double rating) async {
+    Map<String, dynamic> reviews = {
+      'driverId': driverId,
+      'userName': username,
+      'userId': userId,
+      'review': review,
+      'rating': rating
+    };
+    await FirebaseFirestore.instance.collection('reviews').add(reviews);
   }
 }
