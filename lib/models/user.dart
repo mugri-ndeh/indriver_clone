d011b8214cefd5b6d33866d3c566e742263d6c8f
mugri-ndeh <mugrindeh@gmail.com>
2022-01-05 09:21:07 +0100

    added new user properties

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -7,6 +7,9 @@ class UserModel {
   String? username;
   bool? isOnline;
   bool? isDriver;
+  bool? isAdmin;
+  bool? driverAccount;
+  bool? verified;
 
   String? lastName;
   String? email;
@@ -17,47 +20,62 @@ class UserModel {
   String? licenceNo;
   String? carplatenum;
   String? idNo;
+  String? submittedStatus;
 
   int? votes;
   int? trips;
+  double? earnings;
   double? rating;
 
-  UserModel(
-      {this.id,
-      this.firstName,
-      this.username,
-      this.email,
-      this.idNo,
-      this.dob,
-      this.isDriver,
-      this.isOnline,
-      this.lastName,
-      this.licenceNo,
-      this.carplatenum,
-      this.phoneNumber,
-      this.photo,
-      this.rating,
-      this.token,
-      this.trips,
-      this.votes});
+  UserModel({
+    this.id,
+    this.firstName,
+    this.username,
+    this.email,
+    this.isAdmin,
+    this.idNo,
+    this.dob,
+    this.isDriver,
+    this.isOnline,
+    this.lastName,
+    this.licenceNo,
+    this.carplatenum,
+    this.phoneNumber,
+    this.photo,
+    this.rating,
+    this.token,
+    this.trips,
+    this.votes,
+    this.driverAccount,
+    this.submittedStatus,
+    this.verified,
+    this.earnings,
+  });
 
   factory UserModel.fromSnapshot(DocumentSnapshot doc) {
     return UserModel(
-        id: doc['id'],
-        firstName: doc['firstName'],
-        lastName: doc['lastName'],
-        email: doc['email'],
-        username: doc['username'],
-        phoneNumber: doc['phoneNumber'],
-        trips: doc['trips'],
-        photo: doc['photo'],
-        idNo: doc['idNo'],
-        isDriver: doc['isDriver'],
-        isOnline: doc['isOnline'],
-        carplatenum: doc['carplatenum'],
-        rating: doc['rating'],
-        token: doc['token'],
-        votes: doc['votes'],
-        dob: doc['dob']);
+      id: doc['id'],
+      firstName: doc['firstName'],
+      lastName: doc['lastName'],
+      email: doc['email'],
+      username: doc['username'],
+      phoneNumber: doc['phoneNumber'],
+      trips: doc['trips'].toInt(),
+      photo: doc['photo'],
+      idNo: doc['idNo'],
+      isDriver: doc['isDriver'],
+      isOnline: doc['isOnline'],
+      carplatenum: doc['carplatenum'],
+      rating: doc['rating'].toDouble(),
+      token: doc['token'],
+      votes: doc['votes'].toInt(),
+      dob: doc['dob'],
+      licenceNo: doc['licenceNo'],
+      isAdmin: doc['isAdmin'],
+      driverAccount: doc['driverAccount'],
+      verified: doc['verified'],
+      submittedStatus: doc['submittedStatus'],
+      earnings: doc['earnings'].toDouble(),
+    );
   }
 }
